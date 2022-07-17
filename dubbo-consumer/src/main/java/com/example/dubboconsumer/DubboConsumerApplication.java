42fe2b7ceecaec7648cb1004189b4b98346fd46b
gqwd123321 <470124479@qq.com>
2022-07-17 20:40:12 +0800

    after review

--- dubbo-consumer/src/main/java/com/example/dubboconsumer/DubboConsumerApplication.java
+++ dubbo-consumer/src/main/java/com/example/dubboconsumer/DubboConsumerApplication.java
@@ -3,6 +3,8 @@ package com.example.dubboconsumer;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @EnableDubbo
 @SpringBootApplication
@@ -10,6 +12,7 @@ public class DubboConsumerApplication {
 
 
     public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
         SpringApplication.run(DubboConsumerApplication.class, args);
 
     }
